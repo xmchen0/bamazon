d817e5fe2145d4ea0a9d193190fb31121a6cbc0b
Kathy Chen <kathychen.social@gmail.com>
2019-04-07 22:15:20 -0400

    setup purpose and roadmap for app

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,26 +1,73 @@
+/* --------------------------- *\
+|             U of T            |
+|      Coding Bootcamp 2019     |
+|             Bamazon           |
+\* --------------------------- */
+
+/*
+*->> Purpose: Build an Amazon-like store with MySQL and Node.
+    (*) Minimum requirement: The app will take orders from customers and deplete stock from the store's inventory.
+    ( ) Bonus: The app can track product sales across your store's departments and then provide a summary of the highest-grossing departments in the store.
+
+*->> Roadmap: 
+    [1] After installing npm mysql and inquier, import both dependencies via require()
+    [2] Setup connection with mysql with credentials
+    [3] Test connection with a call back function
+
+    [4] View all products available in store via runSearch()
+
+    [5] Customer's order: Prompt customers to choose an item_id corresponding to desired product
+    [6] Then check inventory: Is the selected item there? If not, console log not enough stock
+    [7] Check inventory - might need to loop though the stock inventory to matchup with customer's order
+
+    [8] Quantity: Prompt customers to specify how many quantity they want to purchase
+    [9] If stock is sufficient, make purchase successfully. If not, console log not enough stock, input number again and display catalog
+
+    [10] Customer's receipt: Finalise purchase order
+    [11] Deplete stock quantity from store's inventory 
+    [12] Complete process
+*/
+
+// Require to npm install mysql and inquirer
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+// Setup connection with mysql DB
 var connection = mysql.createConnection({
     host: "localhost",
+    // Your port; if not 3306
     port: 3306,
+    // Your username
     user: "root",
-    password: "Onewayjesus1",
+    // Your password
+    password: "chewbacca",
+    // Database
     database: "bamazonDB"
 });
 
+// Testing connection for any error
 connection.connect(function (err) {
     if (err) throw err;
     runSearch();
 });
 
+// View product catalog
 function runSearch() {
-    inquirer
-        .prompt({
-            item_id: 0,
-            product_name: "product_name",
-            price: 0.00,
-        })
-    console.log(runSearch)
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log(res);
+        console.log(" ");
+        console.log(" Welcome! ");
+        console.log(" Kathy's Boutique Women Store ");
+        console.log(" ");
+        console.log(" Browse catalog here: ");
+        console.table(res);
+        console.log(" ");
+        customerOrder(res);
+    });
+}
 
-};
+// Customer's order
+function customerOrder() {
+
+}
--- bamazonDB.sql
+++ bamazonDB.sql
@@ -8,30 +8,30 @@ USE bamazonDB;
 --   item_id INTEGER NOT NULL AUTO_INCREMENT,
 --   product_name VARCHAR(255) NOT NULL,
 --   department_name VARCHAR(255) NOT NULL,
---   price DOUBLE(8,2) NOT NULL,
+--   price DOUBLE(10,2) NOT NULL,
 --   stock_quantity INTEGER NOT NULL,
 --   PRIMARY KEY (item_id)
 -- );
 
 -- INSERT INTO products(product_name, department_name, price, stock_quantity)
--- VALUES ("shoes", "women's footwear", 120, 2);
+-- VALUES ("shoes", "footwear", 120, 10);
 -- INSERT INTO products(product_name, department_name, price, stock_quantity)
--- VALUES ("socks", "women's footwear", 10, 10);
+-- VALUES ("socks", "footwear", 10, 10);
 -- INSERT INTO products(product_name, department_name, price, stock_quantity)
--- VALUES ("dress", "women's wear", 100, 3);
+-- VALUES ("dresses", "clothing", 100, 30);
 -- INSERT INTO products(product_name, department_name, price, stock_quantity)
--- VALUES ("singlets", "women's wear", 20, 5);
+-- VALUES ("singlets", "clothing", 20, 50);
 -- INSERT INTO products(product_name, department_name, price, stock_quantity)
--- VALUES ("jeans", "women's wear", 100, 2);
+-- VALUES ("jeans", "clothing", 100, 20);
 -- INSERT INTO products(product_name, department_name, price, stock_quantity)
--- VALUES ("hat", "women's wear", 10, 2);
+-- VALUES ("hat", "accessories", 10, 10);
 -- INSERT INTO products(product_name, department_name, price, stock_quantity)
--- VALUES ("beanie", "women's wear", 20, 7);
+-- VALUES ("beanie", "accessories", 20, 7);
 -- INSERT INTO products(product_name, department_name, price, stock_quantity)
--- VALUES ("perfume", "toiletries", 70, 1);
+-- VALUES ("perfume", "toiletries", 70, 10);
 -- INSERT INTO products(product_name, department_name, price, stock_quantity)
--- VALUES ("makeup", "toiletries", 50, 1);
+-- VALUES ("lipstick", "comestics", 50, 5);
 -- INSERT INTO products(product_name, department_name, price, stock_quantity)
--- VALUES ("necklace", "jewellery", 50, 1);
+-- VALUES ("necklace", "jewellery", 50, 5);
 
 -- SELECT * FROM products;
