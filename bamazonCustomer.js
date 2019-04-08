0b4c256ea091898c3825713c11b9799591a062c3
Kathy Chen <kathychen.social@gmail.com>
2019-04-07 23:00:20 -0400

    added customerOrder and checkInventory

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,27 +10,28 @@
     ( ) Bonus: The app can track product sales across your store's departments and then provide a summary of the highest-grossing departments in the store.
 
 *->> Roadmap: 
-    [1] After installing npm mysql and inquier, import both dependencies via require()
+    [1] After installing npm mysql and inqurier, import both dependencies via require()
     [2] Setup connection with mysql with credentials
     [3] Test connection with a call back function
 
     [4] View all products available in store via runSearch()
 
-    [5] Customer's order: Prompt customers to choose an item_id corresponding to desired product
+    [5] Customer's Order: Prompt customers to choose an item_id corresponding to desired product
     [6] Then check inventory: Is the selected item there? If not, console log not enough stock
     [7] Check inventory - might need to loop though the stock inventory to matchup with customer's order
 
     [8] Quantity: Prompt customers to specify how many quantity they want to purchase
     [9] If stock is sufficient, make purchase successfully. If not, console log not enough stock, input number again and display catalog
 
-    [10] Customer's receipt: Finalise purchase order
+    [10] Customer's Receipt: Finalise purchase order
     [11] Deplete stock quantity from store's inventory 
     [12] Complete process
 */
 
-// Require to npm install mysql and inquirer
+// Require to npm install mysql + inquirer + chalk
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var chalk = require("chalk");
 
 // Setup connection with mysql DB
 var connection = mysql.createConnection({
@@ -48,6 +49,7 @@ var connection = mysql.createConnection({
 // Testing connection for any error
 connection.connect(function (err) {
     if (err) throw err;
+    // Initiate
     runSearch();
 });
 
@@ -55,10 +57,14 @@ connection.connect(function (err) {
 function runSearch() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-        console.log(res);
         console.log(" ");
-        console.log(" Welcome! ");
-        console.log(" Kathy's Boutique Women Store ");
+        console.log(" ++++++++++++++++++++++++++++++++++++++++++++ ");
+        console.log(" ");
+        console.log(chalk.bgBlue.white.bold("                 W E L C O M E               "));
+        console.log(" ");
+        console.log("          Kathy's Boutique Women Store        ");
+        console.log(" ");
+        console.log(" ++++++++++++++++++++++++++++++++++++++++++++ ");
         console.log(" ");
         console.log(" Browse catalog here: ");
         console.table(res);
@@ -67,7 +73,51 @@ function runSearch() {
     });
 }
 
-// Customer's order
+// Customer's Order
 function customerOrder() {
+    inquirer
+        .prompt([
+            {
+                name: "choice",
+                type: "input",
+                message: "Like what you see? Type in the item id number of desired product:",
+                validate: function (val) {
+                    return !isNaN(val);
+                    // how to validate user can only input one selection
+                }
+            }
+        ])
+        .then(function (val) {
+            console.log(val);
+            var itemId = parseInt(val.choice);
+            var product = checkInventory(itemId, inventory);
+            if (product) {
+                quantity(product);
+            }
+            else {
+                // make another selection if item id isn't in selection list
+                console.log(" ");
+                console.log(" +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ ");
+                console.log(" ");
+                console.log(" ");
+                console.log(" ");
+                console.log("    We apologise your selection is currently unavailable.    ");
+                console.log("               Please make another selection.                ");
+                console.log(" ");
+                console.log(" ");
+                console.log(" ");
+                console.log(" +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ ");
+                console.log(" ");
+                runSearch();
+            }
+        });
+}
+
+function checkInventory(choiceItem, inventory) {
+    for (var i = 0; i < inventory.length; i++) {
+        if (inventory[i].item_id === choiceItem) {
+            return inventory[i];
+        }
+    } 
 
 }
